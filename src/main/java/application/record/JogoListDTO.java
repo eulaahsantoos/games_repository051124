07c46c26e6d0ab9f6fda2711ef0238b6957a7f8b
MikeeeeVale <mikevale.etec.sp.gov.br@gmail.com>
2024-11-01 12:14:32 +0000

    011124

---
+++ src/main/java/application/record/JogoListDTO.java
@@ -0,0 +1,11 @@
+package application.record;
+
+import application.model.Jogo;
+
+public record JogoListDTO(long id, String titulo) {
+    public JogoListDTO(Jogo jogo) {
+        this(jogo.getId(), jogo.getTitulo());
+    }
+    
+}
+
